[Fix] Placement rotation at sketch creation fixed

NACA4_Generator.py
@@ -50,11 +50,7 @@ class NACA4_Generator:
 		else:
 			return 2*m/math.pow((1-p),2)*(p-x)
 
-#-------------------------
-#builder functions
-#-------------------------
-
-	def builderNACA4(self):
+	def _Xp(self, resolution):
 			
 		Xp = np.linspace(0,1,self.resolution)	#list of points on X Axis equally spaced default
 
@@ -62,6 +58,22 @@ class NACA4_Generator:
 			beta = np.linspace(0,math.pi,self.resolution)
 			for i in range(self.resolution):
 				Xp[i] = 0.5*(1-math.cos(beta[i]))
+		
+		return Xp
+
+#-------------------------
+#builder functions
+#-------------------------
+
+	def builderNACA4(self):
+			
+#		Xp = np.linspace(0,1,self.resolution)	#list of points on X Axis equally spaced default
+
+#		if self.interpolation == 1:	
+#			beta = np.linspace(0,math.pi,self.resolution)
+#			for i in range(self.resolution):
+#				Xp[i] = 0.5*(1-math.cos(beta[i]))
+		Xp = self._Xp(self.resolution)
 
 		Yt	  = np.linspace(0,1,self.resolution)				#list of point for thickness
 		teta	= np.linspace(0,1,self.resolution)					#list of point for gradient
@@ -69,13 +81,15 @@ class NACA4_Generator:
 		Xl	  = np.linspace(0,1,self.resolution)					#list of point on Xlower Axis
 		Yu	  = np.linspace(0,1,self.resolution)					#list of point on Yupper Axis
 		Yl	  = np.linspace(0,1,self.resolution)					#list of point on YLower Axis
+		Yc    = np.linspace(0,1,self.resolution)					#list of point on chamber line
 		if (self.m==0 and self.p ==0):
 			for i in range(self.resolution):
 				Yt[i]   = self._Yt(self.TH,Xp[i])
 				Xu[i]   = Xp[i]*self.chord
 				Xl[i]   = Xp[i]*self.chord
 				Yu[i]   = Yt[i]*self.chord
-				Yl[i]   = Yt[i]*self.chord*(-1)	
+				Yl[i]   = Yt[i]*self.chord*(-1)
+				Yc[i]	= Xp[i] #dummy
 		else:		
 			for i in range(self.resolution):  
 				Yt[i]   = self._Yt(self.TH,Xp[i])
@@ -84,12 +98,13 @@ class NACA4_Generator:
 				Xl[i]   = (Xp[i]+Yt[i]*math.sin(teta[i]))*self.chord
 				Yu[i]   = (self._Yc(Xp[i])+Yt[i]*math.cos(teta[i]))*self.chord
 				Yl[i]   = (self._Yc(Xp[i])-Yt[i]*math.cos(teta[i]))*self.chord
+				Yc[i]	= (self._Yc(Xp[i]))*self.chord
 		
 		#coincident constrain for points on CAD
 		Xl[0] = Xu[0]
-		Yl[0] = Yl[0]
+		Yl[0] = Yu[0]
 		Xl[-1] = Xu[-1]
-		Yl[-1] = Yl[-1]
+		Yl[-1] = Yu[-1]
 		
 
 
@@ -100,11 +115,21 @@ class NACA4_Generator:
 		NACA4.append(Yl)
 		NACA4.append(self.resolution)
 		NACA4.append(str(self.m)+str(self.p)+str(self.TH)+"c"+str(self.chord)+"r"+str(self.resolution))
+		NACA4.append(Yc)
 
 		return NACA4
 
-		def alignOXY (self):
-			pass
+	def alignOXY (self):
+		'''to calculate rotation angle and align the foil to origin'''
+		Xp = self._Xp(self.resolution)		
+		Yt   = self._Yt(self.TH,Xp[-1])
+		teta = math.atan(self._dYc(Xp[-1]))
+		Xu   = (Xp[-1]-Yt*math.sin(teta))*self.chord
+		print (Xu)
+		Yc	= (self._Yc(Xp[-1]))*self.chord
+		print (Yc)
+		beta = math.degrees(math.atan(Yc/Xu)) #alignment angle			
+		return -beta
 
 #--------------------------
 #drawing functions
@@ -114,11 +139,13 @@ def sketchOnPlane(_foil,element,name,_zOffSet):
 
 	_name = name + str(element)
 	_NACA4 = NACA4_Generator.builderNACA4(_foil)
+	_beta = NACA4_Generator.alignOXY(_foil)
+	print (_beta)
 	upperList=[]
 	lowerList=[]
 
 	FreeCAD.activeDocument().addObject('Sketcher::SketchObject',_name)
-	FreeCAD.activeDocument().getObject(_name).Placement = FreeCAD.Placement(FreeCAD.Vector(0.000000,0.000000,_zOffSet),FreeCAD.Rotation(0.000000,0.000000,0.000000,1.000000))
+	FreeCAD.activeDocument().getObject(_name).Placement = FreeCAD.Placement(FreeCAD.Vector(0.000000,0.000000,_zOffSet),FreeCAD.Rotation(FreeCAD.Vector(0.000000,0.000000,1.000000),_beta))
 	FreeCAD.activeDocument().getObject(_name).MapMode = "Deactivated"
 
 	for i in range (_NACA4[4]):
@@ -142,7 +169,8 @@ def sketchOnPlane(_foil,element,name,_zOffSet):
 def airFoilPlot(_foil):
 		'''use matplotlib to plot NACA4 air foil'''	
 		_NACA4 = NACA4_Generator.builderNACA4(_foil)
-		lista = [[],[],[],[]]
+		
+		lista = [[],[],[],[],[]]
 		
 		for j in range (4):
 				for i in range (_NACA4[4]):
@@ -150,10 +178,14 @@ def airFoilPlot(_foil):
 								lista[j].append(_NACA4[j][i])
 						else:
 								lista[j].append(_NACA4[j][i])
+		
+		for i in range (_NACA4[4]):
+			lista[4].append(_NACA4[6][i])
 
 		plt.figure(figsize=(10,2))
 		plt.plot(lista[0],lista[1])
 		plt.plot(lista[2],lista[3])
+		plt.plot(lista[0],lista[4])
 		plt.show()
 
 #-----------------------------------------------------------------------------------------